8e3253e38c30ca037637244519614f14ec5e5b13
高文文 <gaowenwencn@163.com>
2017-12-06 16:08:44 +0800

    此模块的版本信息，默认与依赖的openresty版本一致

---
+++ es_gateway/meta.lua
@@ -0,0 +1,26 @@
+--
+-- User: ¸ßÎÄÎÄ
+-- Date: 2017/12/6
+-- Description: 
+--
+local version = setmetatable({
+    major = 1,
+    minor = 11,
+    patch = 2,
+}, {
+    __tostring = function(t)
+        return string.format("%d.%d.%d%s", t.majot, t.minor, t.patch, t.suffix or "")
+    end
+})
+
+return {
+    _NAME = "es_gateway",
+    _VERSION = tostring(version),
+    _VERSION_TABLE = version,
+
+    -- third-party dependencies required version, as they would be specified
+    _DEPENDENCIES = {
+        openresty = {"1.11.2.2"}
+    }
+}
+
