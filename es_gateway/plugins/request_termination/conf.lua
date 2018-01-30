da6ff7090023f4d05032aa7b6f3918b637a33304
高文文 <gaowenwencn@163.com>
2018-01-30 12:11:38 +0800

    添加基于配置文件的插件：定义插件的一般形式，

---
+++ es_gateway/plugins/request_termination/conf.lua
@@ -0,0 +1,8 @@
+--
+-- User: ╦ънднд
+-- Date: 2018/1/19
+-- Description: 
+--    Request Termination's Configuration, any requests that
+--    -- match the following config item should be terminated!
+--
+
