80b78670d3e95a25b3b883e2de2d5757d795076b
高文文 <gaowenwencn@163.com>
2018-09-26 15:43:51 +0800

    不能将kibana索引名自动加载授权索引中去，需要在UlogPortal中配置 删除：indices[#indices + 1] = kbnName

--- es_gateway/core/processor.lua
+++ es_gateway/core/processor.lua
@@ -41,9 +41,6 @@ local function process()
         handler.preprocess_acl(uri, requestMethod)
         handler.process_request(uri, requestMethod, indices)
     else
-
-        --不能将kibana索引名自动加载授权索引中去，需要在UlogPortal中配置
-        --indices[#indices + 1] = kbnName
         handler.process_kibana_request(uri, kbnName, indices)
     end
 end
